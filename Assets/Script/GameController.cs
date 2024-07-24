5d943d52bd15578a9df3eb227201b00857f38d27
Harry Ho <hemil0102@gmail.com>
2024-07-24 15:46:30 +0900

    player 스폰 및 탁구대 배치

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fusion;
+using TMPro;
+using UnityEngine;
+
+namespace Asteroids.SharedSimple
+{
+	public class GameController : NetworkBehaviour, IPlayerJoined
+	{
+		enum GamePhase
+		{
+			Ready,
+			Starting,
+			Running,
+			Ending
+		}
+
+		[SerializeField] private float _startDelay = 4.0f;
+		[SerializeField] private float _endDelay = 4.0f;
+
+		private TickTimer _dontCheckforWinTimer;
+
+		[Networked] private TickTimer Timer { get; set; }
+		[Networked] private GamePhase Phase { get; set; }
+		[Networked] private NetworkBehaviourId Winner { get; set; }
+
+		public bool GameIsRunning => Phase == GamePhase.Running;
+
+		private SpawnPoint[] _spawnPoints;
+
+		private List<NetworkBehaviourId> _playerDataNetworkedIds = new List<NetworkBehaviourId>();
+
+		private static GameController _singleton;
+
+		public static GameController Singleton
+		{
+			get => _singleton;
+			private set
+			{
+				if (_singleton != null)
+				{
+					throw new InvalidOperationException();
+				}
+				_singleton = value;
+			}
+		}
+
+		private void Awake()
+		{
+			GetComponent<NetworkObject>().Flags |= NetworkObjectFlags.MasterClientObject;
+			Singleton = this;
+		}
+
+		private void OnDestroy()
+		{
+			if (Singleton == this)
+			{
+				_singleton = null;
+			}
+			else
+			{
+				throw new InvalidOperationException();
+			}
+
+		}
+
+		public override void Spawned()
+		{
+			if (Object.HasStateAuthority)
+			{
+				// Initialize the game state on the master client
+				Phase = GamePhase.Ready;
+
+				// 준비 버튼 활성화
+				Debug.Log("준비 버튼 활성화를 해보자!");
+			}
+		}
+
+		public override void Render()
+		{
+			// Update the game display with the information relevant to the current game phase
+			switch (Phase)
+			{
+				case GamePhase.Ready:
+					UpdateReadyDisplay();
+					break;
+
+				case GamePhase.Starting:
+					UpdateStartingDisplay();
+					break;
+				case GamePhase.Running:
+					UpdateRunningDisplay();
+					if (HasStateAuthority)
+					{
+						CheckIfGameHasEnded();
+					}
+					break;
+				case GamePhase.Ending:
+					UpdateEndingDisplay();
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+        private void UpdateReadyDisplay()
+        {
+			FindObjectOfType<PlayerSpawner>().StartPlayerSpawner(this);
+		}
+
+        private void UpdateStartingDisplay()
+		{
+	
+		}
+
+		private void UpdateRunningDisplay()
+		{
+			
+		}
+
+		private void UpdateEndingDisplay()
+		{
+
+		}
+
+		public void CheckIfGameHasEnded()
+		{
+
+
+			GameHasEnded();
+		}
+
+		private void GameHasEnded()
+		{
+			Phase = GamePhase.Ending;
+		}
+
+		public void TrackNewPlayer(NetworkBehaviourId playerDataNetworkedId)
+		{
+			_playerDataNetworkedIds.Add(playerDataNetworkedId);
+		}
+
+		public void PlayerJoined(PlayerRef player)
+		{
+			_dontCheckforWinTimer = TickTimer.CreateFromSeconds(Runner, 5);
+		}
+	}
+}
---
+++ Assets/Script/PlayerSpawner.cs
@@ -0,0 +1,59 @@
+using Fusion;
+using UnityEngine;
+
+namespace Asteroids.SharedSimple
+{
+    // The SpaceshipSpawner, just like the AsteroidSpawner, only executes on the Host.
+    // Therefore none of its parameters need to be [Networked].
+    public class PlayerSpawner : NetworkBehaviour
+    {
+        [Networked] private bool _gameIsReady { get; set; } = false;
+
+        // References to the NetworkObject prefab to be used for the players' spaceships.
+        [SerializeField] private NetworkPrefabRef _playerPrefab = NetworkPrefabRef.Empty;
+
+        private GameController _gameStateController = null;
+
+        private SpawnPoint[] _spawnPoints = null;
+
+        public override void Spawned()
+        {
+            // Collect all spawn points in the scene.
+            _spawnPoints = FindObjectsOfType<SpawnPoint>();
+
+            // When the SpaceshipSpawner gets spawned on a late joiner, spawn a spaceship for them
+            if (_gameIsReady)
+            {
+                SpawnPlayer(Runner.LocalPlayer);
+            }
+        }
+
+        // The spawner is started when the GameController switches to GameState.Running.
+        public void StartPlayerSpawner(GameController gameController)
+        {
+            _gameIsReady = true;
+            _gameStateController = gameController;
+            RpcInitialSpaceshipSpawn();
+            Debug.Log("StartPlayerSpawner 호출");
+        }
+
+        [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+        private void RpcInitialSpaceshipSpawn()
+        {
+            SpawnPlayer(Runner.LocalPlayer);
+        }
+
+        // Spawns a spaceship for a player.
+        // The spawn point is chosen in the _spawnPoints array using the implicit playerRef to int conversion 
+        private void SpawnPlayer(PlayerRef player)
+        {
+            // Modulo is used in case there are more players than spawn points.
+            int index = player.PlayerId % _spawnPoints.Length;
+            var spawnPosition = _spawnPoints[index].transform.position;
+
+            var playerObject = Runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
+            // Set Player Object to facilitate access across systems.
+            Runner.SetPlayerObject(player, playerObject);
+        }
+    }
+}
