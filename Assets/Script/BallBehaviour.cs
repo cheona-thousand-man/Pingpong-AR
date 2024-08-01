a22aa1a62be41e2672c6b17bcd53902cec304cff
cheona-thousand-man <cheon1000aa@gmail.com>
2024-08-01 10:22:23 +0900

    update BallBehabour

--- Assets/Script/BallBehaviour.cs
+++ Assets/Script/BallBehaviour.cs
@@ -28,19 +28,29 @@ public class BallBehaviour : NetworkBehaviour
 
     public void FixedUpdateNetworked()
     {
-        CheckBallDropOrStopped();
-    }
-
-    private void CheckBallDropOrStopped()
-    {
-        throw new NotImplementedException();
+        if (CheckBallStopped())
+        {
+            AddScoreToOwner();
+            StartCoroutine(DroppedBallDespawn());
+        }
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
+            // 충돌한 객체에서 NetworkObject 가져오기
+            var networkObject = other.gameObject.GetComponent<NetworkObject>();
+            if (networkObject != null)
+            {
+                // NetworkObject에서 PlayerDataNetworked NetworkBehaviour를 가져오기
+                var playerDataNetworked = networkObject.GetComponent<PlayerDataNetworked>();
+                if (playerDataNetworked != null)
+                {
+                    PlayerRef playerRef = NetworkUtilities.GetPlayerRefFromNetworkBehaviourId(Runner, playerDataNetworked.Id);
+                    SetOwner(playerRef);
+                }
+            }
         }
         else if (other.gameObject.CompareTag("TableUp"))
         {
@@ -56,7 +66,57 @@ public class BallBehaviour : NetworkBehaviour
         }
         else if (other.gameObject.CompareTag("Court"))
         {
-            
+            AddScoreToOwner();
+            StartCoroutine(DroppedBallDespawn());
         }
+    }
+
+    private bool CheckBallStopped()
+    {
+        return _rigidbody.velocity == Vector3.zero && _rigidbody.angularVelocity == Vector3.zero;
+    }
+
+    public void SetOwner(PlayerRef playerRef)
+    {
+        Owner = playerRef;
+    } 
+    
+    public void AddScoreToOwner()
+    {
+        // Owner가 Player1일 경우
+        if (Owner.Equals(NetworkUtilities.GetPlayerRefFromNetworkBehaviourId(Runner, _gameController.Player1)))
+        {
+            // NetworkBehaviourId에서 NetworkBehaviour를 찾아 점수 +1
+            Runner.TryFindBehaviour<PlayerDataNetworked> (_gameController.Player1, out var playerDataNetworked);
+            playerDataNetworked.AddToScore();
+
+            // 공을 놓친 플레이어에게 서브 권한 전달
+            ServePlayerChange(_gameController.Player2);
+        }
+        // Owner가 Player2일 경우
+        else if (Owner.Equals(NetworkUtilities.GetPlayerRefFromNetworkBehaviourId(Runner, _gameController.Player2)))
+        {
+             // NetworkBehaviourId에서 NetworkBehaviour를 찾아 점수 +1
+            Runner.TryFindBehaviour<PlayerDataNetworked> (_gameController.Player1, out var playerDataNetworked);
+            playerDataNetworked.AddToScore();
+
+            // 공을 놓친 플레이어에게 서브 권한 전달
+            ServePlayerChange(_gameController.Player1);
+        }
+    }
+
+    public void ServePlayerChange(NetworkBehaviourId nextServePlayer)
+    {
+        var playerRef = NetworkUtilities.GetPlayerRefFromNetworkBehaviourId(Runner, nextServePlayer);
+        _gameController.Rpc_SetServePlayer(playerRef);
+    }
+
+    IEnumerator DroppedBallDespawn()
+    {
+        // 1초 경과 후 Despawn 실행
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log($"바닥에 떨어진 공 디스폰: {Object.Id}");
+        Runner.Despawn(Object);
     }
 }
